2e90c20f18ebcd545cc267be02eef687dce9c192
fxdomcloud <fxdomcloud@gmail.com>
2024-11-08 10:08:32 +0800

    init

---
+++ a_files/custom.js
@@ -0,0 +1,195 @@
+﻿
+var _fileUploadIsSend = false;
+var _fileUploadIsSend2 = true;
+//載入頁面統一要做的事
+$(function ()
+{
+
+})
+
+$(window).resize(function () {
+    
+});
+
+//變更預設分頁筆數
+function InputGo(page) {
+    $('div.pager').find('input[type=text]').each(function () {
+		$(this).val(page);
+	});
+}
+//送出變更分頁筆數
+function ChangeDefaultPage() {
+	SendSubmit();
+}
+//轉至url
+function GoUrl(url) {
+	location.href = url;
+}
+
+//cehck url replace &符號
+function CheckUrl(url) {
+    url = url.replace(/&amp;/g, '&');
+    if (url.indexOf('?') > -1) {
+        url += "&";
+    }
+    else {
+        url += "?";
+    }
+    return url;
+}
+//送出第一個form or 指定
+function SendSubmit(index) {
+    index = index || 0;
+    $("form")[index].submit();
+}
+
+//回上一頁
+function back() {
+    if (1 < history.length) {
+        history.back();
+        return false;
+    }
+    return true;
+}
+
+//對urlParameter編碼
+function EncodeParameter(value) {
+	return encodeURIComponent(value);
+}
+//縮圖
+function DrawImage(ImgD, FitWidth, FitHeight) {
+	var image = new Image();
+	image.src = ImgD.src;
+	if (image.width > 0 && image.height > 0) {
+		if (image.width / image.height >= FitWidth / FitHeight) {
+			if (image.width > FitWidth) {
+				ImgD.width = FitWidth;
+				ImgD.height = (image.height * FitWidth) / image.width;
+			} else {
+				ImgD.width = image.width;
+				ImgD.height = image.height;
+			}
+		} else {
+			if (image.height > FitHeight) {
+				ImgD.height = FitHeight;
+				ImgD.width = (image.width * FitHeight) / image.height;
+			} else {
+				ImgD.width = image.width;
+				ImgD.height = image.height;
+			}
+		}
+	}
+
+}
+//檢查上下線日期
+function CheckDateUpDown(date1, date2)
+{
+    if ($.datepicker.parseDate("yy/mm/dd", date1) >
+                $.datepicker.parseDate("yy/mm/dd", date2)) {
+        alert("日期範圍輸入錯誤！！");
+        return false;
+    }
+}
+
+//移除id
+function Remove(id) {
+	$("#" + id).remove();
+}
+//關閉 BlockUI
+function CloseBlockUI() {
+	$.unblockUI();
+}
+//取得網址#後的部分
+function UrlReplace(url) {
+	return url.replace(/^.*#/, '');
+}
+
+//給FileUpload 用的送出
+function StartSendSubmitByFileUpload(sec) {
+    sec = sec || 1000;
+    var _canSend = true;
+    $(".uploadii").each(function () {
+        if ($(this).val() != "1") {
+            _canSend = false;
+            return false;
+        }
+    });
+    //$("div.ajax-file-upload-container").each(function () {
+    //    if ($(this).find('div.ajax-file-upload-statusbar').length > 0) {
+    //        _canSend = false;
+    //        return false;
+    //    }
+    //});
+    if (_canSend) {
+        SendSubmit();//基本上會有2個form 所以要送第1個
+        //if (!_fileUploadIsSend) {
+        //    _fileUploadIsSend = true;
+        //    SendSubmit();//基本上會有2個form 所以要送第1個
+        //}
+    }
+    else {
+        setInterval("StartSendSubmitByFileUpload()", sec);
+    }
+}
+
+//檢查cookie是否開啟
+function CheckCookie() {
+	$.cookie('checkCookie', 'test', { path: '/' });
+	if ($.cookie('checkCookie') == null) {
+		alert('已關閉瀏覽器Cookie功能，部分網站功能無法正常使用!!');
+	}
+	else {
+		$.cookie('checkCookie', null);
+	}
+}
+
+//取得int
+function GetInt(value) {
+    return parseInt(value) || 0;
+}
+
+//取得float
+function GetFloat(value) {
+    return parseFloat(value) || 0;
+}
+
+//計算小數
+function GetFormatFloat(num, pos) {
+    var size = Math.pow(10, pos);
+    return Math.round(num * size) / size;
+}
+
+//show提示對話框
+//msg 要提示的訊息
+//msgType 1=success ,2=notice ,3=warning ,4=error 
+function showMsg(msg, msgType) {
+	switch (msgType) {
+		case 1:
+			msgType="success"
+			break;
+		case 2:
+			msgType = "notice"
+			break;
+		case 3:
+			msgType = "warning"
+			break;
+		case 4:
+			msgType = "error"
+			break;
+		default:
+			msgType = "notice"
+			break;
+
+	}
+	$().toastmessage('showToast', {
+		text: msg,
+		stayTime: 3000,
+		sticky: false,
+		position: 'middle-center',//訊息位置：1.top-left  2.top-center 3.top-right 4.middle-left 5.middle-center 6.middle-right
+		type: msgType, //success ,notice ,warning ,error 
+		closeText: '',
+		close: function () {
+
+		}
+	});
+}
---
+++ files/main.js
@@ -0,0 +1,208 @@
+﻿//主banner動畫
+    $(document).ready(function() {
+        var wSize = $(window).width();
+        var hSize = $(window).height();
+
+        //runner,people
+        var runnerW = wSize * 0.03384
+        var peopleW = wSize * 0.03538
+        var carW = wSize * 0.12307
+        $(".runnerR").width(runnerW);
+        $(".walkerRight").width(runnerW);
+        $(".walkerLeft").width(runnerW);
+        $(".walkerR3").width(runnerW);
+        $(".walkerL3").width(runnerW);
+        $(".travelerL").width(runnerW);
+        $(".travelerR").width(runnerW);
+        $(".carL").width(carW);
+        $(".carR").width(carW);
+        $(".people").width(peopleW);
+
+        //people
+        randomPos();
+
+        function randomPos() {
+            $(".people").each(function() {
+                var peopleRdmPos = Math.round(Math.random() * 80);
+                var peopleRdmPos10 = peopleRdmPos + 10
+                $(this).css({ left: peopleRdmPos10 + "%" })
+            });
+        }
+
+        //runner
+        var runnerRdmPos = Math.round(Math.random() * 50);
+        var runnerRdmPosDis = 100 - runnerRdmPos
+        $(".Bridge .runner").css({ left: runnerRdmPos + "%" });
+        $(".Bridge .runner").animate({ left: 100 + "%" }, runnerRdmPosDis * 120, "linear", function() {
+            $(".Bridge .runner").css({ left: -3.384 + "%" });
+            runnerLoop();
+        });
+
+        function runnerLoop() {
+            setInterval(function() {
+                var rdmDelay = Math.round(Math.random() * 40000);
+                $(".Bridge .runner").delay(rdmDelay + 4000).animate({ left: 50 + "%" }, 8000, "linear").animate({ left: 100 + "%", bottom: 0 }, 8000, "linear", function() {
+                    $(".Bridge .runner").css({ left: -3.384 + "%" });
+                });
+            }, 100);
+        }
+
+        //runnerR
+        setInterval(function() {
+            var rdmDelay = Math.round(Math.random() * 40000);
+            $(".Bridge .runnerR").delay(rdmDelay + 4000).animate({ left: 50 + "%" }, 8000, "linear").animate({ left: -3.384 + "%" }, 8000, "linear", function() {
+                $(".Bridge .runnerR").css({ left: 100 + "%" });
+            });
+        }, 100);
+
+        //walkerRight
+
+        var walkerRightRdmPos = Math.round(Math.random() * 50);
+        var walkerRightRdmPosDis = 100 - walkerRightRdmPos
+        $(".Bridge .walkerRight").css({ left: walkerRightRdmPosDis + "%" });
+        $(".Bridge .walkerRight").animate({ left: -3.384 + "%" }, walkerRightRdmPosDis * 420, "linear", function() {
+            $(".Bridge .walkerRight").css({ left: 100 + "%" });
+            walkerRightLoop();
+        });
+
+        function walkerRightLoop() {
+            setInterval(function() {
+                var rdmDelay = Math.round(Math.random() * 20000);
+                $(".Bridge .walkerRight").delay(rdmDelay + 4000).animate({ left: 50 + "%" }, 17000, "linear").animate({ left: -3.384 + "%" }, 17000, "linear", function() {
+                    $(".Bridge .walkerRight").css({ left: 100 + "%" });
+                });
+            }, 100);
+        }
+
+        //walkerLeft
+
+        var walkerLeftRdmPos = Math.round(Math.random() * 50);
+        var walkerLeftRdmPosDis = 100 - walkerLeftRdmPos
+        $(".Bridge .walkerLeft").css({ left: walkerLeftRdmPos + "%" });
+        $(".Bridge .walkerLeft").animate({ left: 100 + "%" }, walkerLeftRdmPosDis * 460, "linear", function() {
+            $(".Bridge .walkerLeft").css({ left: -3.384 + "%" });
+            walkerLeftLoop();
+        });
+
+        function walkerLeftLoop() {
+            setInterval(function() {
+                var rdmDelay = Math.round(Math.random() * 20000);
+                $(".Bridge .walkerLeft").delay(rdmDelay + 4000).animate({ left: 50 + "%" }, 20000, "linear").animate({ left: 100 + "%" }, 20000, "linear", function() {
+                    $(".Bridge .walkerLeft").css({ left: -3.384 + "%" });
+                });
+            }, 100);
+        }
+
+        //walkerR3
+        setInterval(function() {
+            var rdmDelay = Math.round(Math.random() * 20000);
+            $(".Bridge .walkerR3").delay(rdmDelay + 4000).animate({ left: 50 + "%" }, 18000, "linear").animate({ left: -3.384 + "%" }, 18000, "linear", function() {
+                $(".Bridge .walkerR3").css({ left: 100 + "%" });
+            });
+        }, 100);
+
+        //walkerL3
+
+        var walkerL3RdmPos = Math.round(Math.random() * 50);
+        var walkerL3RdmPosDis = 100 - walkerL3RdmPos
+        $(".Bridge .walkerL3").css({ left: walkerL3RdmPos + "%" });
+        $(".Bridge .walkerL3").animate({ left: 100 + "%" }, walkerL3RdmPosDis * 400, "linear", function() {
+            $(".Bridge .walkerL3").css({ left: -3.384 + "%" });
+            walkerL3Loop();
+        });
+
+        function walkerL3Loop() {
+            setInterval(function() {
+                var rdmDelay = Math.round(Math.random() * 20000);
+                $(".Bridge .walkerL3").delay(rdmDelay + 4000).animate({ left: 50 + "%" }, 18000, "linear").animate({ left: 100 + "%" }, 18000, "linear", function() {
+                    $(".Bridge .walkerL3").css({ left: -3.384 + "%" });
+                });
+            }, 100);
+        }
+
+        //travelerL
+        setInterval(function() {
+            var rdmDelay = Math.round(Math.random() * 20000);
+            $(".Bridge .travelerL").delay(rdmDelay + 4000).animate({ left: 50 + "%" }, 19000, "linear").animate({ left: 100 + "%" }, 19000, "linear", function() {
+                $(".Bridge .travelerL").css({ left: -3.384 + "%" });
+            });
+        }, 100);
+
+        //travelerR
+
+        var travelerRRdmPos = Math.round(Math.random() * 50);
+        var travelerRRdmPosDis = 100 - travelerRRdmPos
+        $(".Bridge .travelerR").css({ left: travelerRRdmPos + "%" });
+        $(".Bridge .travelerR").animate({ left: -3.384 + "%" }, travelerRRdmPos * 460, "linear", function() {
+            $(".Bridge .travelerR").css({ left: 100 + "%" });
+            travelerRLoop();
+        });
+
+        function travelerRLoop() {
+            setInterval(function() {
+                var rdmDelay = Math.round(Math.random() * 20000);
+
+            }, 100);
+        }
+
+    });
+
+    $(window).resize(function() {
+        var wSize = $(window).width();
+        var hSize = $(window).height();
+
+        var runnerW = wSize * 0.03384
+        var peopleW = wSize * 0.03538
+        var carW = wSize * 0.12307
+        $(".runnerR").width(runnerW);
+        $(".walkerRight").width(runnerW);
+        $(".walkerLeft").width(runnerW);
+        $(".walkerR3").width(runnerW);
+        $(".walkerL3").width(runnerW);
+        $(".travelerL").width(runnerW);
+        $(".travelerR").width(runnerW);
+        $(".people").width(peopleW);
+    });
+
+     $(function() {
+        $(".menu-btn").click(function() {
+            $(".nav").addClass("focus");
+            $(".header").addClass("focus");
+            $(".black").addClass("focus");
+            $("body").addClass("focus");
+            $("html").addClass("focus");
+        });
+        $(".black").click(function() {
+            $(".nav").removeClass("focus");
+            $(".header").addClass("focus");
+            $(".black").removeClass("focus");
+            $("body").removeClass("focus");
+            $("html").removeClass("focus");
+        });
+
+        $(".toggle").trigger("click");
+
+    });
+    if ($(window).width() < 1024) {
+
+        $(function() {
+            $(".inner-box > a").click(function() {
+                if ($(this).prop("class") != "focus") {
+                    $(".inner-box > a").removeClass("focus");
+                    $(".second").removeClass("focus");
+                    $(this).addClass("focus");
+                    $(this).next(".second").addClass("focus");
+                } else {
+                    $(".inner-box > a").removeClass("focus");
+                    $(".second").removeClass("focus");
+                }
+            });
+        });
+    }
+
+    $(function() {
+        
+
+    });
+
+    
